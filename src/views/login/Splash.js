d1a824b8fef9b81562e5f555edafd911ccce6d7e
ricardo <ricardo.izasiga@gmail.com>
2017-02-17 11:40:14 -0500

    try_social_form

--- src/views/login/Splash.js
+++ src/views/login/Splash.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import logo from '../../assets/img/logoMusic.png'
+import google from '../../assets/img/google.png'
+import spotify from '../../assets/img/spotify2.png'
 import Header from './Header'
 import Nav from '../shared/Nav'
 import '../../assets/css/login.css'
 import { browserHistory } from 'react-router'
 
 class Body extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      login: false
+    }
+  }
   onClick(e){
     browserHistory.push('/onboarding')
   }
+  showForm(value){
+    this.setState({
+      login: true
+    });
+  }
   render() {
-    return (
-      <div className="App">
-        <center>
-          <div><img src={logo} alt="" className="login-logo"/></div>
+    const  social = (
+          <div className="form-container percent-80">
+            <button type="button" className="social-button" onClick={this.onClick.bind(this)}>
+              <i className="fa fa-facebook"></i>Ingresa con facebook
+            </button>
+            <button type="button" className="social-btn google spotify">
+              <img src={spotify} alt="google" />Ingresa con Spotify
+            </button>
+            <button type="button" className="social-btn google" onClick={this.showForm.bind(this)}>
+              <img src={google} alt="google" />Ingresa con Google
+            </button>
+          </div>
+      );
+    const form = (
           <div className="form-container percent-80">
             <div className="input-group">
               <span className="input-group-addon transparent">
@@ -34,6 +57,12 @@ class Body extends Component {
             <div className="text-divider" ><a href="#"  
             onClick={this.onClick.bind(this)}>¿Olvidaste tu contraseña?</a></div>
           </div>
+      );
+    return (
+      <div className="App">
+        <center>
+          <div><img src={logo} alt="" className="login-logo"/></div>
+          { this.state.login ? form : social }
         </center>
       </div>
     );
